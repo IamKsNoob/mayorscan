e5f49c34954952cea0f9c296a0139957b3150e85
Joe Helle <25853389+dievus@users.noreply.github.com>
2020-01-05 17:54:24 -0500

    Update mayorscan.sh

--- mayorscan.sh
+++ mayorscan.sh
@@ -6,7 +6,7 @@ if [ "$1" == "" ]
 	then
 	echo -e "\e[31mYou forgot a target!\e[0m"
 	echo -e "\e[31mExample - ./mayorscan.sh 192.168.1.1 or cybersecpadawan.com\e[0m"
-exit 0 
+	exit 0 
 fi
 
 echo "Enter 1 for Single File"
@@ -15,9 +15,9 @@ read scan
 if [ "$scan" = "1" ] || [ "$scan" = "2" ]
 	then
 	echo -e "\e[32mNow running The Mayor's Nmap Scan 3000. Running a Mandatory Ping Scan against target(s)...\e[0m"
-else
+	else
 	echo -e "\e[31mFailure.  Please enter either a 1 or a 2.\e[0m"
-exit 0
+	exit 0
 fi
 
 if [ ! -d "$name" ];then
@@ -27,12 +27,12 @@ fi
 if [ "$scan" = "1" ]
 	then
 	nmap -Pn $name >&1 | tee $name/$name.FullScan.txt
-elif [ "$scan" = "2" ]
+	elif [ "$scan" = "2" ]
 	then 
 	nmap -Pn $name >&1 | tee $name/$name.PingScan.txt
 fi
 
-#Software Scan#
+#Software Scan
 
 echo -e "\e[32m-----------------------------------------------------------\e[0m"
 echo -e "Do you wish to conduct a Software Version Scan? (y/n)?"
@@ -48,7 +48,7 @@ if [ "$ans" = "y" ] && [ "$scan" = "2" ]
 elif [ "$ans" = "y" ] && [ "$scan" = "1" ]
 	then
 	nmap -sV $name >&1 | tee -a $name/$name.FullScan.txt	
-else [ "$ans" = "n" ]
+	else [ "$ans" = "n" ]
 fi
 
 #OS System Scanning
@@ -64,10 +64,10 @@ fi
 if [ "$ans" = "y" ] && [ "$scan" = "2" ]
 	then
 	nmap -O $name >&1 | sort -u | tee $name/$name.OperatingSystemScan.txt
-elif [ "$ans" = "y" ] && [ "$scan" = "1" ]
+	elif [ "$ans" = "y" ] && [ "$scan" = "1" ]
 	then
 	nmap -O $name >&1 | tee -a $name/$name.FullScan.txt
-else [ "$ans2" = "n" ]
+	else [ "$ans2" = "n" ]
 fi
 
 #Vulnerability Assessment Scanning
