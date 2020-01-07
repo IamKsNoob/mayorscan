6be98f6a75c808d5ceb91a9bb4663f90093cdf40
Joe Helle <25853389+dievus@users.noreply.github.com>
2020-01-06 22:05:59 -0500

    Update mayorscan-lite.sh

--- mayorscan-lite.sh
+++ mayorscan-lite.sh
@@ -50,19 +50,20 @@ tput setaf 2; echo -e "#####################################"; tput sgr0;
 echo -e
 tput bold; echo -e "Running Ping Scan to check if host is up."; tput sgr0;
 echo -e
-NMAP=$(sudo nmap -Pn $name)
-if [[ $NMAP | grep "Host is down" ]]; then
-tput setaf 1; tput bold; echo -e "No Live Host found.  Exiting now."; tput sgr0;
-else
-echo
-fi
-#>&1 | tee $name/$name.FullScan.txt
+sudo nmap -Pn $name >&1 | tee $name/$name.FullScan.txt
+
 echo -e
 tput setaf 2; echo -e "#####################################"; tput sgr0;
 echo -e
 tput bold; echo -e "Conducting Software Version Scan"; tput sgr0;
 echo -e
 sudo nmap -sV $name >&1 | tee -a $name/$name.FullScan.txt
+file=$name.Fullscan.txt
+if [ cat $file | grep "Host is down" ]; then
+tput bold; tput setaf 1; echo -e "No live host found. Exiting now."; tput sgr0;
+exit 0
+else echo
+fi
 echo -e
 tput setaf 2; echo -e "#####################################"; tput sgr0;
 echo -e
